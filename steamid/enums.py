80f12e1a9b5207900976518740bdf30e60e92751
suke <sukekeai@gmail.com>
2024-02-26 01:57:45 +0800

    添加 SteamID 类

---
+++ steamid/enums.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+
+from enum import Enum, IntEnum, IntFlag
+from re import compile as re_compile
+from typing import Pattern
+
+
+class Universes(IntEnum):
+    invalid  = 0
+    public   = 1
+    beta     = 2
+    interval = 3
+    dev      = 4
+    # rc     = 5
+
+
+class Types(IntEnum):
+    invalid          = 0   # 不属于其他类别的机器人和帐号
+    individual       = 1   # 单用户
+    multiseat        = 2   # 多用户 如网吧
+    game_server      = 3   # 游戏服务器
+    anon_game_server = 4   # 匿名游戏服务器
+    pending          = 5   # 单用户帐号在等待 steam 验证期间的临时状态
+    content_server   = 6   # 内容服务器
+    clan             = 7   # steam 群组
+    chat             = 8   # 聊天，包含 多用户聊天T、群组聊天c、steam大厅聊天L
+    console_user     = 9   # PS3 的本地帐号和 XBOX360 live 帐号等 使用的虚假帐号
+    anon_user        = 10  # 匿名用户
+
+    P2P_super_seeder = console_user
+
+
+class TypeChars(IntEnum):
+    I = Types.invalid.value
+    U = Types.individual.value
+    M = Types.multiseat.value
+    G = Types.game_server.value
+    A = Types.anon_game_server.value
+    P = Types.pending.value
+    C = Types.content_server.value
+    g = Types.clan.value
+    T = Types.chat.value      # 多用户聊天
+    a = Types.anon_user.value
+
+    i = I
+
+    c = T                     # 群组聊天
+    L = T                     # steam 大厅聊天
+
+
+class Instances(IntEnum):
+    all     = 0
+    desktop = 1
+    console = 2
+    web     = 4
+
+
+def _bit_fill_1(bit_lenght: int) -> int:
+    """3 -> 0b111"""
+    return (1 << bit_lenght) - 1
+
+
+class BitLenght(IntEnum):
+    full        = 64
+
+    universe    = 8
+    type        = 4
+    instance    = 20
+    account_id  = 32
+
+    account_pre = 31
+    account_suf = 1
+
+    auth_server = account_suf
+
+
+class Masks(IntFlag):
+    type        = _bit_fill_1(BitLenght.type)
+    universe    = _bit_fill_1(BitLenght.universe)
+    instance    = _bit_fill_1(BitLenght.instance)
+    account_pre = _bit_fill_1(BitLenght.account_pre)
+    account_suf = _bit_fill_1(BitLenght.account_suf)
+
+
+# 聊天使用的特殊 instance，使用前面的 bit
+class InstanceFlags(IntFlag):
+    chat_clan      = (Masks.instance + 1) >> 1
+    chat_lobby     = (Masks.instance + 1) >> 2
+    chat_MMS_lobby = (Masks.instance + 1) >> 3
+
+
+class MaxValue(IntEnum):
+    account_pre = _bit_fill_1(BitLenght.account_pre)
+    account_suf = _bit_fill_1(BitLenght.account_suf)
+    account_id  = _bit_fill_1(BitLenght.account_id)
+    steam_id    = _bit_fill_1(BitLenght.full)
+
+
+# 除了 U, g, c, L, T，这几个（个人、群组、聊天），其他应该都是不准的
+# 因为在不同的地方看到了不同的写法（一个是置0一个是置1）
+# 而且使用匿名游戏服务器测试发现，instance 不是固定值，几个服务器的都不相同
+class TypeCharsToInstances(IntEnum):
+    I = Instances.all
+    U = Instances.desktop
+    M = Instances.all
+    G = Instances.all
+    A = Instances.all
+    P = Instances.all
+    C = Instances.all
+    g = Instances.all
+    T = Instances.all
+    a = Instances.all
+
+    i = I
+
+    c = InstanceFlags.chat_clan
+    L = InstanceFlags.chat_lobby
+
+
+class Patterns(Enum):
+    # \d{0, 20}
+    steamID64: Pattern = re_compile('^[0-9]{0,20}$')  # f'^[0-9]{{0,{len(str(MaxValue.steam_id))}}}$'
+    steamID: Pattern = steamID64
+
+    # STEAM_X:Y:Z
+    steamID32: Pattern = re_compile('^STEAM_(?P<universe>[0-5]):(?P<account_suf>[0-1]):(?P<account_pre>[0-9]{1,10})$')
+    steamID2: Pattern = steamID32
+
+    # [N:1:M[:O]]
+    steamID3: Pattern = re_compile(
+        '^\\[(?P<type>[AGMPCgcLTIUai]):(?P<universe>[0-4]):(?P<account_id>[0-9]{0,10})(?::(?P<instance>[0-9]+))?]$')
---
+++ steamid/main.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+
+# https://github.com/xPaw/SteamID.php
+# https://steam.readthedocs.io/en/stable/api/steam.steamid.html
+# https://github.com/SteamRE/SteamKit/blob/master/SteamKit2/SteamKit2/Types/SteamID.cs
+
+from .enums import (
+    BitLenght,
+    Instances,
+    Masks,
+    MaxValue,
+    Patterns,
+    TypeChars,
+    TypeCharsToInstances,
+    Types,
+    Universes,
+)
+
+
+class SteamID:
+    def __init__(self, steam_id: str | int = None) -> None:
+
+        self._universe: Universes = Universes.invalid
+        self._type: Types = Types.invalid
+        self._instance: Instances = Instances.all
+        self._account_id: int = 0
+
+        if steam_id is not None:
+            self.set_id(steam_id)
+
+    def set_id(self, steam_id: str | int) -> None:
+        steam_id = str(steam_id)
+        if result := Patterns.steamID64.value.match(steam_id):
+            self._parse_steamID64(result.string)
+        elif Patterns.steamID32.value.match(steam_id):
+            self._parse_steamID32(steam_id)
+        elif Patterns.steamID3.value.match(steam_id):
+            self._parse_steamID3(steam_id)
+
+    @staticmethod
+    def _divide_account_id(account_id: int) -> tuple[int, int]:
+        return account_id >> BitLenght.account_suf, account_id & Masks.account_suf
+
+    @staticmethod
+    def _combine_account_id(account_pre: int, account_suf: int) -> int:
+        if account_pre > MaxValue.account_pre:
+            raise ValueError(f'account_pre 超过了最大值：{MaxValue.account_pre}')
+        if account_suf > MaxValue.account_suf:
+            raise ValueError(f'account_suf 超过了最大值：{MaxValue.account_suf}')
+        return (account_pre << BitLenght.account_suf) | account_suf
+
+    @staticmethod
+    def _shift_right_read(num: int, length: int) -> tuple[int, int]:
+        mask = (1 << length) - 1
+        result = num & mask
+        num >>= length
+        return result, num
+
+    def _parse_steamID64(self, steam_id64: str | int) -> None:
+        steam_id64 = int(steam_id64)
+        if steam_id64 > MaxValue.steam_id:
+            raise ValueError(f'传入数据不是有效的 steamID')
+        num = steam_id64
+        self._account_id, num = self._shift_right_read(num, BitLenght.account_id)
+        self._instance, num = self._shift_right_read(num, BitLenght.instance)
+        self._type, num = self._shift_right_read(num, BitLenght.type)
+        self._universe, num = self._shift_right_read(num, BitLenght.universe)
+
+    def _parse_steamID32(self, steam_id: str) -> None:
+        result = Patterns.steamID32.value.match(steam_id)
+
+        account_pre = int(result.group('account_pre'))
+        account_suf = int(result.group('account_suf'))
+        account_id = self._combine_account_id(account_pre, account_suf)
+        if account_id > MaxValue.account_id:
+            raise ValueError(f'account_id 超过了最大值：{MaxValue.account_id}')
+        self._account_id = account_id
+
+        universe = int(result.group('universe'))
+        # 由于某些错误，在一些旧的游戏中获取 id 时，universe 显示为 0，实际上应该是 1
+        if universe == Universes.invalid:
+            universe = Universes.public
+        self._universe = universe
+
+        self._instance = Instances.desktop
+        self._type = Types.individual
+
+    def _parse_steamID3(self, steam_id: str) -> None:
+        result = Patterns.steamID3.value.match(steam_id)
+
+        account_id = int(result.group('account_id'))
+        if account_id > MaxValue.account_id:
+            raise ValueError(f'account_id 超过了最大值：{MaxValue.account_id}')
+        self._account_id = account_id
+
+        type_ = result.group('type')
+        self._type = Types(TypeChars[type_]).name
+
+        universe = result.group('universe')
+        self._universe = universe
+
+        instance = result.group('instance')
+        if instance is None or self._type == Types.chat:
+            instance = TypeCharsToInstances[type_]
+        self._instance = instance
+
+    @property
+    def steam_id64(self) -> str:
+        shift_dis_account_id = 0
+        shift_dis_instance = shift_dis_account_id + BitLenght.account_id
+        shift_dis_type = shift_dis_instance + BitLenght.instance
+        shift_dis_universe = shift_dis_type + BitLenght.type
+        return str(
+            (self._universe << shift_dis_universe) |
+            (self._type << shift_dis_type) |
+            (self._instance << shift_dis_instance) |
+            (self._account_id << shift_dis_account_id)
+        )
+
+    @property
+    def steam_id(self) -> str:
+        return self.steam_id64
+
+    @property
+    def steam_id32(self) -> str:
+        account, auth_server = self._divide_account_id(self._account_id)
+        return f'STEAM_{self._universe}:{auth_server}:{account}'
+
+    @property
+    def steam_id2(self) -> str:
+        return self.steam_id32
+
+    @property
+    def steam_id3(self) -> str:
+        extra = ''
+        if self._type == Types.multiseat or (self._type == Types.individual and self._instance != Instances.desktop):
+            extra = f':{self._instance}'
+        return f'[{TypeChars(self._type).name}:{self._universe}:{self._account_id}{extra}]'
+
+    @property
+    def is_valid(self) -> bool:
+        if self._universe == Universes.invalid or self._universe not in Universes:
+            return False
+        if self._type == Types.invalid or self._type not in Types:
+            return False
+        if self._type == Types.individual and (
+                self._account_id == 0 or self._instance not in Instances):
+            return False
+        if self._type == Types.clan and (
+                self._account_id == 0 or self._instance != 0):
+            return False
+        if self._type == Types.game_server and self._account_id == 0:
+            return False
+
+        return True
+
+    @property
+    def is_valid_user(self) -> bool:
+        if self._universe == Universes.invalid or self._universe not in Universes:
+            return False
+        if ((self._type == Types.individual or self._type == Types.console_user)
+                and self._account_id != 0 and self._instance in Instances):
+            return True
+        return False
+
+    def __repr__(self):
+        return (f'<{self.__class__} '
+                f'universe={self._universe} '
+                f'type={self._type} '
+                f'instance={self._instance} '
+                f'account_id={self._account_id} '
+                f'>')
